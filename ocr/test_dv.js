b0f40dded05eb05014e176b44c1ca4480bde0a87
Your Name <you@example.com>
2015-09-06 01:05:23 +0300

    learn learn learn...

--- ocr/test_dv.js
+++ ocr/test_dv.js
@@ -1,7 +1,27 @@
 
 var dv = require('dv');
 var fs = require('fs');
-var image = new dv.Image('jpg', fs.readFileSync('/Users/albert/Desktop/trader-joe.jpg'));
-// var image = new dv.Image('jpg', fs.readFileSync('/Users/albert/Desktop/trader-joe_orig.jpg'));
+// var image = new dv.Image('jpg', fs.readFileSync('./trader-joe.jpg'));
+var image = new dv.Image('jpg', fs.readFileSync('./trader-joe-orig.jpg'));
 var tesseract = new dv.Tesseract('eng', image);
+
 console.log(tesseract.findText('plain'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
