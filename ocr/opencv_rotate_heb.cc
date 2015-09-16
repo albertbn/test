249dcd6a48a37af77272e4967549bd2895d618ce
albertbn <albert.bentov@gmail.com>
2015-09-16 11:51:44 +0300

    ok, progress with rotation... see next what happens...

---
+++ ocr/opencv_rotate_heb.cc
@@ -0,0 +1,49 @@
+
+// g++ $(pkg-config --cflags --libs opencv)opencv_rotate_heb.cc -oopencv_rotate_heb && ./opencv_rotate_heb
+
+#include "opencv2/opencv.hpp"
+
+// testing: http://felix.abecassis.me/2011/10/opencv-rotation-deskewing/
+void deskew(const char* filename, double angle)
+{
+  cv::Mat img = cv::imread(filename, 0);
+
+  cv::bitwise_not(img, img);
+
+  std::vector<cv::Point> points;
+  cv::Mat_<uchar>::iterator it = img.begin<uchar>();
+  cv::Mat_<uchar>::iterator end = img.end<uchar>();
+  for (; it != end; ++it)
+    if (*it)
+      points.push_back(it.pos());
+
+  cv::RotatedRect box = cv::minAreaRect(cv::Mat(points));
+
+  cv::Mat rot_mat = cv::getRotationMatrix2D(box.center, angle, 1);
+
+  cv::Mat rotated;
+  cv::warpAffine(img, rotated, rot_mat, img.size(), cv::INTER_CUBIC);
+
+  cv::Size box_size = box.size;
+  if (box.angle < -45.)
+    std::swap(box_size.width, box_size.height);
+  cv::Mat cropped;
+  cv::getRectSubPix(rotated, box_size, box.center, cropped);
+
+  cv::imshow("Original", img);
+  cv::imshow("Rotated", rotated);
+  cv::imshow("Cropped", cropped);
+  cv::waitKey(0);
+}
+
+int main ( int argc, char** argv )
+{
+  // compute_skew( "./pics/heb.jpg" );
+  // deskew( "./img_pre/heb.jpg", -11.3953 );
+  // deskew( "./img_pre/heb.jpg", 11.3953 );
+  // deskew( "./img_pre/heb.jpg", 292.0 ); /*this is working???*/
+  deskew( "./img_pre/heb.jpg", 258.6047 ); /*this is working???*/
+  // deskew( "./img_pre/heb.jpg", 348.6 );
+
+  return 0;
+}
---
+++ ocr/opencv_skew_heb.cc
@@ -0,0 +1,47 @@
+
+// g++ $(pkg-config --cflags --libs opencv)opencv_skew_heb.cc -oopencv_skew_heb && ./opencv_skew_heb
+
+#include "opencv2/opencv.hpp"
+
+// testing: http://felix.abecassis.me/2011/09/opencv-detect-skew-angle/
+void compute_skew(const char* filename)
+{
+  // Load in grayscale.
+  cv::Mat src = cv::imread(filename, 0);
+  cv::Size size = src.size();
+
+  cv::bitwise_not(src, src);
+
+  std::vector<cv::Vec4i> lines;
+
+  cv::HoughLinesP(src, lines, 1, CV_PI/180, 100, size.width / 2.f, 20);
+
+  cv::Mat disp_lines(size, CV_8UC1, cv::Scalar(0, 0, 0));
+  double angle = 0.;
+  unsigned nb_lines = lines.size();
+  for (unsigned i = 0; i < nb_lines; ++i)
+    {
+      cv::line(disp_lines, cv::Point(lines[i][0], lines[i][1]),
+               cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255, 0 ,0));
+      angle += atan2((double)lines[i][3] - lines[i][1],
+                     (double)lines[i][2] - lines[i][0]);
+    }
+  angle /= nb_lines; // mean angle, in radians.
+
+  std::cout << "angle in radians: " << angle << std::endl;
+  std::cout << "File " << filename << ": " << angle * 180 / CV_PI << std::endl;
+
+  cv::imwrite( "./img_pre/heb_ocv_angle.jpg", disp_lines);
+
+  // cv::imshow(filename, disp_lines);
+  // cv::waitKey(0);
+  // cv::destroyWindow(filename);
+}
+
+int main ( int argc, char** argv )
+{
+  // compute_skew( "./pics/heb.jpg" ); /*don't - use origininal image - use grayscaled/threholded + reduce size...*/
+  compute_skew( "./img_pre/heb.jpg" );
+
+  return 0;
+}
