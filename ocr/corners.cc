d355d272ac453e775b91ec82befed5b601bdc9dc
Your Name <you@example.com>
2015-09-22 08:51:36 +0300

    trying with corner detect...
    keep gun up, over water,may the force be with you

--- ocr/corners.cc
+++ ocr/corners.cc
@@ -17,7 +17,9 @@ using namespace std;
 // http://stackoverflow.com/questions/6555629/algorithm-to-detect-corners-of-paper-sheet-in-photo
 void corners()
 {
-   Mat mat = imread( "./pics/heb.jpg");
+   // Mat mat = imread( "./pics/heb.jpg");
+   Mat mat = imread( "./pics/heb_new.jpg");
+   // Mat mat = imread( "./pics/heb2.jpg");
 
    cv::cvtColor(mat, mat, CV_BGR2GRAY);
    cv::GaussianBlur(mat, mat, cv::Size(3,3), 0);
@@ -31,11 +33,15 @@ void corners()
    std::vector<cv::Vec4i> lines;
    lines.clear();
    cv::HoughLinesP(edges, lines, 1, CV_PI/180, 25);
+
+   std::cout << "lines count: " << lines.size()  << std::endl;
+
    std::vector<cv::Vec4i>::iterator it = lines.begin();
    for(; it!=lines.end(); ++it) {
      cv::Vec4i l = *it;
      cv::line(edges, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255,0,0), 2, 8);
    }
+
    std::vector< std::vector<cv::Point> > contours;
    cv::findContours(edges, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_TC89_KCOS);
    std::vector< std::vector<cv::Point> > contoursCleaned;
@@ -43,16 +49,32 @@ void corners()
      if (cv::arcLength(contours[i], false) > 100)
        contoursCleaned.push_back(contours[i]);
    }
-   std::vector<std::vector<cv::Point> > contoursArea;
 
+   std::cout << "contoursCleaned count: " << contoursCleaned.size()  << std::endl;
+
+   std::vector<std::vector<cv::Point> > contoursArea;
    for (int i=0; i < contoursCleaned.size(); i++) {
-     if (cv::contourArea(contoursCleaned[i]) > 10000){
+     if (cv::contourArea(contoursCleaned[i]) > 5000){
        contoursArea.push_back(contoursCleaned[i]);
+       std::cout << "carea points count: " << contoursCleaned[i].size() << std::endl;
      }
    }
+
+   std::cout << "contoursArea count (this is where the animal stinks): " <<  contoursArea.size()  << std::endl;
+
    std::vector<std::vector<cv::Point> > contoursDraw (contoursCleaned.size());
+   // Mat drawing = Mat::zeros( mat.size(), CV_8UC3 );
    for (int i=0; i < contoursArea.size(); i++){
      cv::approxPolyDP(Mat(contoursArea[i]), contoursDraw[i], 40, true);
+
+     std::cout << "draw lines count: " << contoursDraw[i].size() << std::endl;
+
+     // std::vector<std::vector<cv::Point> > ctemp;
+     // ctemp.push_back(contoursDraw[i]);
+     // cv::drawContours(drawing, ctemp, -1, cv::Scalar(0 , 0, 255),1);
+     // std::ostringstream filename;
+     // filename <<  "./img_pre/cor0" << i << ".jpg";
+     // cv::imwrite( filename.str(), drawing);
    }
    Mat drawing = Mat::zeros( mat.size(), CV_8UC3 );
    cv::drawContours(drawing, contoursDraw, -1, cv::Scalar(0,255,0),1);
