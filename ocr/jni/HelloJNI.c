cec1b5f987650c0aa4e5232a1f4c4bd44ab7dc10
Your Name <you@example.com>
2015-12-25 22:05:40 +0200

    going on with strugle - compile a simple test JNI...
    may the force be with you,
    
    suck Shawn, suck

---
+++ ocr/jni/HelloJNI.c
@@ -0,0 +1,10 @@
+
+#include <jni.h>
+#include <stdio.h>
+#include "HelloJNI.h"
+
+JNIEXPORT void JNICALL Java_HelloJNI_sayHello
+(JNIEnv *env, jobject thisObj){
+  printf("\n\nFuck the blacks\n\n");
+  return;
+}
