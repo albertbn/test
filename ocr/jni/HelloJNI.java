a8c0c32c55fc534dc1e5889cd628c0482caa28f0
albertbn <albertbn>
2015-12-27 12:01:56 +0200

    yep! simple hammer and wire :)
    go on and test the JNI fuck out of it...
    
    may the force be with you,
    
    suck Shawn, suck

--- ocr/jni/HelloJNI.java
+++ ocr/jni/HelloJNI.java
@@ -1,8 +1,33 @@
 
-public class HelloJNI{
+//javac HelloJNI.java && java HelloJNI
+
+// set #JAVA_HOME
+// http://www.mkyong.com/java/how-to-set-java_home-environment-variable-on-mac-os-x/
+
+// vim ~/.bash_profile
+// export JAVA_HOME=$(/usr/libexec/java_home)
+// source .bash_profile
+
+// JNI tute
+// https://www3.ntu.edu.sg/home/ehchua/programming/java/JavaNativeInterface.html
+
+// http://stackoverflow.com/questions/1403788/java-lang-unsatisfiedlinkerror-no-dll-in-java-library-path
+// Rick C. Hodgin - thanks man, load the focking file
+
+//yep!
+
+import java.lang.*;
+import java.io.File;
+
+public class HelloJNI {
 
     static {
-        System.loadLibrary("hello");
+
+        // System.out.println(System.getProperty("java.library.path"));
+        File f;
+        f = new File("hello.so");
+        // System.loadLibrary("hello");
+        System.load(f.getAbsolutePath());
     }
 
     private native void sayHello();
