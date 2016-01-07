ea3cb64af9a4a58ab8bdf7c1d24417b9aa3cd56c
Your Name <you@example.com>
2016-01-07 22:48:37 +0200

    hard with java env
    may the force be with you,
    
    suck Shawn, suck

--- ocr/evdroid/src/diordve/bonebou/MainActivity.java
+++ ocr/evdroid/src/diordve/bonebou/MainActivity.java
@@ -1,6 +1,12 @@
+
 package diordve.bonebou;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import org.apache.http.NameValuePair;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -34,5 +40,51 @@ public class MainActivity extends Activity {
         // Toast toast = Toast.makeText(context, text, duration);
         // toast.show();
         preNocr.main( null );
+    }
+
+        //============
+    //main fn to send errors
+    //============
+    void post_error ( final String text ) {
+
+        try {
+            //=================
+            Thread t = new Thread( new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try{
+                            ArrayList<NameValuePair> nameValuePairs = new  ArrayList<NameValuePair>();
+                            nameValuePairs.add(new BasicNameValuePair("text",text));
+
+                            HttpClient httpclient = new DefaultHttpClient();
+                            HttpPost httppost = new HttpPost(UPLOAD_URL);
+                            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+                            //HttpResponse response =
+                            httpclient.execute(httppost);
+
+                        } catch( Exception e ){
+
+                            //check what to do with error
+                            //show_msg(e.toString());
+                        }
+                    }
+                });
+
+            t.start();
+            //=================
+        } catch ( Exception ex ) {
+            //e.printStackTrace();
+        }
+    }
+
+    public String err_str ( Exception ex ) {
+
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter( writer );
+        ex.printStackTrace( printWriter );
+        printWriter.flush();
+
+        return writer.toString();
     }
 }
