d46f324e54666708fa91e282260da3af35e502d6
albertbn <albertbn>
2016-02-08 10:06:51 +0200

    go on with angles... could be problematic
    may the force be with you,
    
    suck Shawn, suck

--- ocr/evdroid/jni/util/rect_poly.cc
+++ ocr/evdroid/jni/util/rect_poly.cc
@@ -40,13 +40,14 @@ float get_angle_avg_by_lengths ( Mat_<float> angles,  vector<double> len_contour
   return angles_sum_by_len/divide_by;
 }
 
-// TODO - go on from here - something stinky here? 6 Feb 2016
+// something stinky here? 6 Feb 2016
 void get_closest_diagonal ( Mat_<float> angles, vector< std::vector<cv::Point> > contours, Mat &pic, vector<double> len_contours ) {
 
   float angle_avg = mean(angles)[0]; /*calculate, to know if horizontal or vertical*/
   bool is_vert = is_vertical(angle_avg);
+  cout << "get_closest_diagonal :: angle_avg, is_vert: " << angle_avg << ',' << is_vert << endl;
 
-  vector<cv::Point> points = get_points_from_contours(is_vert, contours);
+  vector<cv::Point> points = get_points_from_contours ( is_vert, contours );
 
   cout << "\n\n=========\nget_closest_diagonal :: angles, points\n" << '\n' << angles << '\n' << points << endl;
   // vx,vy,x,y
@@ -59,9 +60,26 @@ void get_closest_diagonal ( Mat_<float> angles, vector< std::vector<cv::Point> >
   float x = line_result[2];
   float y = line_result[3];
 
-  float angle__avg_by_len =  get_angle_avg_by_lengths ( angles, len_contours );
+  float angle_avg_by_len =  get_angle_avg_by_lengths ( angles, len_contours );
   //TODO - go on from x = 1291 * atan2(180-angle... vert hor rad - see get_max_deviation in common)
-  cout << "get_closest_diagonal :: angle__avg_by_len : " << angle__avg_by_len << endl;
+  if ( is_vert ) {
+    float ang = 180.0 - angle_avg_by_len;
+    cout << "get_closest_diagonal :: ang degrees: " << ang << endl;
+    ang = abs(ang*CV_PI/180.0); /* to rad */
+    cout << "get_closest_diagonal :: ang rad: " << ang << endl;
+    cout << "get_closest_diagonal :: tan rad: " << tan(ang) << endl;
+    cout << "get_closest_diagonal :: cotan rad: " << 1.0/tan(ang) << endl;
+      // abs(angle_avg_by_len * 180 / CV_PI);
+    float x_upper = y * 1.0/tan(ang) + x ;
+    float x_lower = x - (size_mat.height-y) * 1.0/tan(ang);
+    cout << "get_closest_diagonal ::  upper point DIY :" << Point(x_upper,0) << endl;
+    cout << "get_closest_diagonal ::  lower point DIY :" << Point(x_lower,size_mat.height) << endl;
+
+    cv::circle ( pic, Point(x_upper,0), 50, cv::Scalar(255,255,255) );
+    cv::circle ( pic, Point(x_lower,size_mat.height), 50, cv::Scalar(255,255,255) );
+  }
+
+  cout << "get_closest_diagonal :: angle__avg_by_len : " << angle_avg_by_len << endl;
   cout << "get_closest_diagonal :: vx, vy, x, y : " << vx << ',' << vy << ',' << x << ',' << y << endl;
 
   float x0, y0, x1, y1;
@@ -89,7 +107,7 @@ void get_closest_diagonal ( Mat_<float> angles, vector< std::vector<cv::Point> >
   cout << "get_closest_diagonal :: x0, y0, x1, y1 : " << x0 << ',' << y0 << ',' << x1 << ',' << y1  << endl;
   if ( lines4intersect_validate( is_vertical(angle_avg), Point(x,y), Vec4i(x0,y0,x1,y1) ) ) {
 // #ifndef ANDROID
-    // cv::line ( pic, Point(x0, y0), Point(x1, y1), cv::Scalar(0,64,255), 2, CV_AA ); /*boost performance*/
+    cv::line ( pic, Point(x0, y0), Point(x1, y1), cv::Scalar(0,64,255), 2, CV_AA ); /*boost performance*/
 // #endif // ANDROID
   }
 }
