2a6fc361d447e8267a7e76f02c329084143223ae
albertbn <albertbn>
2016-02-29 10:25:55 +0200

    added the MyRealFuck...ok, should have made the first initial fast and shame merge for frame poreview + hq click
    may the force be with you
    
    suck Shawn, suck

--- ocr/MyRealTimeImageProcessing/src/my/project/MyRealTimeImageProcessing/CameraPreview.java
+++ ocr/MyRealTimeImageProcessing/src/my/project/MyRealTimeImageProcessing/CameraPreview.java
@@ -1,4 +1,3 @@
-
 /*
  *  CameraPreview.java
  */
@@ -10,6 +9,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.ImageView;
 
 import android.graphics.Bitmap;
@@ -17,8 +17,9 @@ import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 
-public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCallback
-{
+public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    SurfaceHolder mHolder;
     Camera mCamera = null;
     ImageView MyCameraPreview = null;
     Bitmap bitmap = null;
@@ -31,8 +32,15 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
 
     Handler mHandler = new Handler(Looper.getMainLooper());
 
-    public CameraPreview(int PreviewlayoutWidth, int PreviewlayoutHeight,
-                         ImageView CameraPreview) {
+    public CameraPreview (
+                         int PreviewlayoutWidth,
+                         int PreviewlayoutHeight,
+                         ImageView CameraPreview,
+                         Camera camera,
+                         SurfaceHolder holder) {
+        mCamera = camera;
+        mHolder = holder;
+
         PreviewSizeWidth = PreviewlayoutWidth;
         PreviewSizeHeight = PreviewlayoutHeight;
         MyCameraPreview = CameraPreview;
@@ -40,8 +48,9 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
         pixels = new int[PreviewSizeWidth * PreviewSizeHeight];
     }
 
+    // Indian thanks and bows man - that's about all I needed
     @Override
-    public void onPreviewFrame(byte[] arg0, Camera arg1) {
+    public void onPreviewFrame ( byte[] arg0, Camera arg1 ) {
         // At preview mode, the frame data will push to here.
         if (imageFormat == ImageFormat.NV21) {
             //We only accept the NV21(YUV420) format.
@@ -57,7 +66,8 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+    public void surfaceChanged ( SurfaceHolder arg0, int arg1, int arg2, int arg3 ) {
+
         Parameters parameters = mCamera.getParameters();
         // Set the camera preview size
         parameters.setPreviewSize(PreviewSizeWidth, PreviewSizeHeight);
@@ -67,34 +77,59 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder arg0) {
+    public void surfaceCreated ( SurfaceHolder arg0 ) {
         mCamera = Camera.open();
         if(mCamera==null) mCamera = Camera.open(0);
-        try
-            {
-                // If did not set the SurfaceHolder, the preview area will be black.
-                mCamera.setPreviewDisplay(arg0);
-                mCamera.setPreviewCallback(this);
-            }
-        catch (IOException e)
-            {
-                mCamera.release();
-                mCamera = null;
-            }
+        try {
+            // If did not set the SurfaceHolder, the preview area will be black.
+            mCamera.setPreviewDisplay(arg0);
+            mCamera.setPreviewCallback(this);
+        }
+        catch (IOException e) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder arg0) {
+    public void surfaceDestroyed ( SurfaceHolder arg0 ) {
         mCamera.setPreviewCallback(null);
         mCamera.stopPreview();
         mCamera.release();
         mCamera = null;
     }
 
-    //
+    public void refreshCamera ( Camera camera ) {
+
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch ( Exception e ) {
+            // ignore: tried to stop a non-existent preview
+        }
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setCamera(camera);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            Log.d("MyRealTimeImageProcessing", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void setCamera ( Camera camera ) {
+        //method to set a camera instance
+        mCamera = camera;
+    }
+
     // Native JNI
-    //
-    public native boolean ImageProcessing(int width, int height, byte[] NV21FrameData, int [] pixels);
+    public native boolean ImageProcessing ( int width, int height, byte[] NV21FrameData, int [] pixels );
 
     static {
         System.loadLibrary("pngt");
@@ -104,6 +139,7 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
     }
 
     Runnable DoImageProcessing = new Runnable() {
+
             public void run() {
                 Log.i("MyRealTimeImageProcessing", "DoImageProcessing():");
                 bProcessing = true;
--- ocr/MyRealTimeImageProcessing/src/my/project/MyRealTimeImageProcessing/MyRealTimeImageProcessing.java
+++ ocr/MyRealTimeImageProcessing/src/my/project/MyRealTimeImageProcessing/MyRealTimeImageProcessing.java
@@ -1,4 +1,15 @@
+
 package my.project.MyRealTimeImageProcessing;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,22 +20,38 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MyRealTimeImageProcessing extends Activity
-{
-        CameraPreview camPreview;
-        ImageView MyCameraPreview = null;
-        FrameLayout mainLayout;
-        int PreviewSizeWidth = 640;
-        int PreviewSizeHeight= 480;
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.hardware.Camera.PictureCallback;
+import android.content.pm.PackageManager;
+import android.widget.Button;
 
-   @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+public class MyRealTimeImageProcessing extends Activity {
+
+    Camera mCamera;
+    CameraPreview camPreview;
+    PictureCallback mPicture;
+    Button capture;
+    Context myContext;
+    ImageView MyCameraPreview = null;
+    FrameLayout mainLayout;
+    int PreviewSizeWidth = 640;
+    int PreviewSizeHeight= 480;
+
+    @Override
+    public void onCreate ( Bundle savedInstanceState ) {
+
         super.onCreate(savedInstanceState);
         //Set this APK Full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                                                        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // from andrew cam example - other project
+        myContext = this;
+
         //Set this APK no title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
@@ -34,7 +61,7 @@ public class MyRealTimeImageProcessing extends Activity
 
         SurfaceView camView = new SurfaceView(this);
         SurfaceHolder camHolder = camView.getHolder();
-        camPreview = new CameraPreview(PreviewSizeWidth, PreviewSizeHeight, MyCameraPreview);
+        camPreview = new CameraPreview(PreviewSizeWidth, PreviewSizeHeight, MyCameraPreview, mCamera, camHolder);
 
         camHolder.addCallback(camPreview);
         camHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -42,11 +69,123 @@ public class MyRealTimeImageProcessing extends Activity
         mainLayout = (FrameLayout) findViewById(R.id.frameLayout1);
         mainLayout.addView(camView, new LayoutParams(PreviewSizeWidth, PreviewSizeHeight));
         mainLayout.addView(MyCameraPreview, new LayoutParams(PreviewSizeWidth, PreviewSizeHeight));
+        capture = (Button) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captrureListener);
     }
 
-    protected void onPause() {
+    @Override
+    protected void onPause ( ) {
+
         if ( camPreview != null)
             camPreview.onPause();
         super.onPause();
+
+        // from andrew cam example - other project
+        //when on Pause, release camera in order to be used from other applications
+        releaseCamera();
+    }
+
+    @Override
+    public void onResume ( ) {
+
+        super.onResume();
+        if ( !hasCamera(myContext) ) {
+            Toast toast =
+                Toast.makeText(myContext,
+                               "Sorry, your phone does not have a camera!",
+                               Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if ( mCamera == null ) {
+
+            mCamera = Camera.open(0);
+            mPicture = getPictureCallback();
+            camPreview.refreshCamera(mCamera);
+        }
+    }
+
+    //set form onCreate
+    OnClickListener captrureListener = new OnClickListener() {
+            @Override
+            public void onClick ( View v ) {
+
+                Camera.Parameters params = mCamera.getParameters();
+                params.setPictureSize(2048, 1536);
+                params.setJpegQuality(100);
+                mCamera.setParameters(params);
+                mCamera.takePicture ( null, null, mPicture ) ;
+            }
+        };
+
+    //callback - trace - from captureListener > onClick > cam.takePicture
+    PictureCallback getPictureCallback ( ) {
+        PictureCallback picture = new PictureCallback ( ) {
+
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    //make a new picture file
+                    File pictureFile = getOutputMediaFile();
+
+                    if (pictureFile == null) {
+                        return;
+                    }
+                    try {
+                        //write the file
+                        FileOutputStream fos = new FileOutputStream(pictureFile);
+                        fos.write(data);
+                        fos.close();
+                        Toast toast =
+                            Toast.makeText(
+                                           myContext,
+                                           "Picture saved: " + pictureFile.getName(),
+                                           Toast.LENGTH_LONG);
+                        toast.show();
+
+                    } catch (FileNotFoundException e) {
+                    } catch (IOException e) {
+                    }
+
+                    //refresh camera to continue preview
+                    camPreview.refreshCamera ( mCamera );
+                }
+            };
+        return picture;
+    }
+
+    //create file path where the .jpg will be saved
+    static File getOutputMediaFile() {
+
+        //make a new file directory inside the "sdcard" folder
+        File mediaStorageDir = new File("/sdcard/", "JCG Camera");
+
+        //if this "JCGCamera folder does not exist
+        if (!mediaStorageDir.exists()) {
+            //if you cannot make this folder return
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        //take the current timeStamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        //and make a media file:
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    void releaseCamera ( ) {
+        // stop and release camera
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    boolean hasCamera ( Context context ) {
+        //check if the device has camera
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 }
