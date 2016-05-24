5ac9596f39c160f3f360c22a5ab492f56ae480e3
albertbn <albertbn>
2016-05-24 11:13:34 +0300

    added Arial, results not so good for arial cinemateque - better for HQ... think next
    may the force be with you,
    
    suck Shawn, suck

--- ocr/train/heb/miriam1/train.py
+++ ocr/train/heb/miriam1/train.py
@@ -20,8 +20,8 @@ if do_train:
     # arr_fonts_ext = sys.argv[1:]
     # ! DON'T forget to add fonts to font_properties
     arr_fonts_ext = [
-        'heb.engCourierNew.exp0.tiff ',
-        'heb.miriam.exp0.tiff', 'heb.courierNew.exp0.tiff'
+        # 'heb.engCourierNew.exp0.tiff ',
+        'heb.miriam.exp0.tiff', 'heb.courierNew.exp0.tiff', 'heb.arial.exp0.tiff'
         ,'heb.kadosh.exp0.jpg', 'heb.kadosh.exp1.jpg', 'heb.kadosh.exp2.jpg'
         ,'heb.half_free.exp0.jpg', 'heb.supersal.exp0.jpg', 'heb.lahmanina.exp0.jpg'
     ]
